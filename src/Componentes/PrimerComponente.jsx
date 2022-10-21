6e8652753ddb560e77f2ca8deb178dd245f4b03d
daniel quelca <quelca61s@gmail.com>
2022-10-21 11:48:30 -0400

    segundo componente

--- src/Componentes/PrimerComponente.jsx
+++ src/Componentes/PrimerComponente.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PrimerComponente() {
   return (
-    <div>
+    <div className='data'>
         <span className='name'>Dianna</span>
         <span className='description'>
         Has maiorum habemus detraxit at. Timeam fabulas splendide et his.Usu nullam dolorum quaestio ei, sit vidit facilisis ea. Per ne impedit iracundia neglegentur.
